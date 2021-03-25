45892d241b0cda5d67e70ca18cdecf854957fbb1
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-03-25 01:31:34 +0000

    inicializacion

---
+++ src/ejericio-1.ts
@@ -0,0 +1,11 @@
+/**
+ * Esta funcion sumara dos numeros
+ * @param valor1 recibe un number 
+ * @param valor2 recibe un number
+ * @returns retorna el resultado de la suma de tipo number
+ */
+export function add(valor1: number, valor2: number): number {
+  return valor1 + valor2;
+}
+
+console.log(add(5,6));
