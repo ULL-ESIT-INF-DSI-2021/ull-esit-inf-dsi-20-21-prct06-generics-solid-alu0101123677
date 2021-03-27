433957caa454e9d8fb4f34ee09be2e77094c2a5d
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-03-27 18:39:59 +0000

    ejercicio 1 0.2

---
+++ src/ejercicio-1/superclass_fighter.ts
@@ -0,0 +1,4 @@
+export abstract class Fighter {
+  constructor(protected name: string, protected height: number, protected weight: number,
+    protected attack: number, protected defense: number, protected catching_phrase: string){}
+}
---
+++ tests/ejercicio-1/subclass_pokemon.spec.ts
@@ -0,0 +1,14 @@
+import 'chai';
+import {expect} from 'chai';
+import {Fighter} from '../../src/ejercicio-1/superclass_fighter';
+import {Pokemon} from '../../src/ejercicio-1/subclass_pokemon';
+
+describe('check Fighter class', () => {
+  let Pikachu = new Pokemon("Pikachu", 1.0, 2.0, 30, 40, "pika pika");
+  it('comprobacion si Pikachu es de la clase Pokemon', () => {
+    expect(Pikachu).to.be.an.instanceOf(Pokemon);
+  });
+  it('comprobacion de si pikachu recibe herencia de la clase Fighter', () => {
+    expect(Pikachu).to.be.an.instanceOf(Fighter);
+  });
+});
