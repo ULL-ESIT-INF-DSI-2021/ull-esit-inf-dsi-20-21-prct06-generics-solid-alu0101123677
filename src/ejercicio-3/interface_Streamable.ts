b17122241203de598c1cdacb6c71ba72e76e3379
Bruno Arroyo <alu0101123677@ull.edu.es>
2021-03-31 13:03:59 +0100

    ejercicio 3 0.4

--- src/ejercicio-3/interface_Streamable.ts
+++ src/ejercicio-3/interface_Streamable.ts
@@ -1,4 +1,4 @@
 
-export interface Streamble<> {
+export interface Streamble<T> {
     
 }
--- src/ejercicio-3/subclass_documentary.ts
+++
@@ -1,3 +0,0 @@
-export class Documentary {
-  constructor(protected title: string, protected director: string, protected actor: string, protected year: number){}
-}
